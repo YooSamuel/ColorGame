fd8ed258bf7fc79351ce39300b8ea886efb91b5c
AidenHellenaChoi <aidenchoi0729@gmail.com>
2019-03-21 20:07:42 +0900

    조금 만들어 놨어요
    ㅎㅎㅎ

---
+++ ColorProject/Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public Text secondsPlayingText;
+    float secondsPlaying = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("IncreaseSecond", 0f, 0.1f);
+
+    }
+    void IncreaseSecond()
+    {
+        secondsPlaying += 0.1f;
+        secondsPlayingText.text = secondsPlaying.ToString();
+    }
+
+
+
+    void Update()
+    {
+
+    }
+}
